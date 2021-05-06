d51a1e7ae0a4966f7d45adc95405e994d4c95b7a
DanGuzVan <danielholosguzman@gmail.com>
2021-05-05 19:17:10 -0500

    Create DetalleVentasService.java

---
+++ src/main/java/cursoDAgil/service/detalleventas/DetalleVentasService.java
@@ -0,0 +1,9 @@
+package cursoDAgil.service.detalleventas;
+
+import java.util.List;
+
+import cursoDAgil.bd.domain.DetalleVentas;
+
+public interface DetalleVentasService {
+	List<DetalleVentas> listarTodasDetalleVentas();
+}
