0f810677529e26784e782e0bf98587b27b1334ff
DanGuzVan <danielholosguzman@gmail.com>
2021-05-05 19:20:35 -0500

    Create DetalleVentasServiceImplTest.java

---
+++ src/test/java/cursoDAgil/service/detalleventas/DetalleVentasServiceImplTest.java
@@ -0,0 +1,39 @@
+package cursoDAgil.service.detalleventas;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cursoDAgil.bd.domain.DetalleVentas;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/applicationContext.xml"})
+
+public class DetalleVentasServiceImplTest {
+	
+	@Inject
+	DetalleVentasService detalleVentasService;
+	
+	@Test
+	public void pruebaConsultarTodo(){
+		try{
+			List<DetalleVentas> lista = detalleVentasService.listarTodasDetalleVentas();
+			assertEquals(lista.size(),4);
+			for(DetalleVentas d: lista){
+				System.out.println("ventaId: " + d.getVentaId());
+				System.out.println("prodictoId: " +  d.getProductoId());
+				System.out.println("cantidad: " + d.getCantidad());
+			}
+		}catch(Exception e){
+			System.out.println("Error: " + e);
+		}
+	}
+
+}
