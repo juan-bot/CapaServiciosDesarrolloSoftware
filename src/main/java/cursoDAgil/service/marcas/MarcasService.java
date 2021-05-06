e642418a7e53973abb10e6f5c0336f72d9cc4cf6
DanGuzVan <danielholosguzman@gmail.com>
2021-05-05 19:19:15 -0500

    Create MarcasService.java

---
+++ src/main/java/cursoDAgil/service/marcas/MarcasService.java
@@ -0,0 +1,14 @@
+package cursoDAgil.service.marcas;
+
+import java.util.List;
+import java.util.Map;
+
+import cursoDAgil.bd.domain.Marcas;
+
+public interface MarcasService {
+	List<Marcas> listarTodasMarcas();
+	Marcas listarMarcaPorId(Map<String, Integer> mapMarcas);
+	Integer nuevaMarca(Marcas marca);
+	Integer eliminaMarca(Map<String, Integer> mapMarcas);
+	Integer actualizaMarca(Marcas marca);
+}
