5c1668f71c72e8617f6562f1c395aad491f544fa
luisaandreamg <56168185+luisaandreamg@users.noreply.github.com>
2021-05-06 13:18:18 -0500

    Update ClienteService.java

--- src/main/java/cursoDAgil/service/cliente/ClienteService.java
+++ src/main/java/cursoDAgil/service/cliente/ClienteService.java
@@ -1,9 +1,14 @@
 package cursoDAgil.service.cliente;
 
 import java.util.List;
+import java.util.Map;
 
 import cursoDAgil.bd.domain.Cliente;
 
 public interface ClienteService {
 	List<Cliente> listarTodosClientes();
+	Integer nuevoCliente(Cliente cliente);
+	Integer editarCliente(Cliente cliente);
+	Cliente obtenerClientePorId(Map<String, Integer> mapCliente);
+	Integer eliminarCliente(Map<String, Integer> mapCliente);
 }
