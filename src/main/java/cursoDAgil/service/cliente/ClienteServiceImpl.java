06d7a06ff6e0b78921a56286f4729d8015939410
luisaandreamg <56168185+luisaandreamg@users.noreply.github.com>
2021-05-06 13:19:00 -0500

    Update ClienteServiceImpl.java

--- src/main/java/cursoDAgil/service/cliente/ClienteServiceImpl.java
+++ src/main/java/cursoDAgil/service/cliente/ClienteServiceImpl.java
@@ -2,6 +2,7 @@ package cursoDAgil.service.cliente;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -10,10 +11,12 @@ import cursoDAgil.bd.domain.Cliente;
 import cursoDAgil.dao.cliente.ClienteDao;
 
 @Named
-public class ClienteServiceImpl implements ClienteService, Serializable{
+public class ClienteServiceImpl implements ClienteService, Serializable {
 
-	private static final long serialVersionUID = 7962397593863792875L;
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Inject
 	ClienteDao clienteDao;
 	
@@ -21,5 +24,27 @@ public class ClienteServiceImpl implements ClienteService, Serializable{
 	public List<Cliente> listarTodosClientes() {
 		return clienteDao.listarTodosClientes();
 	}
+
+	@Override
+	public Integer nuevoCliente(Cliente cliente) {
+		return clienteDao.nuevoCliente(cliente);
+	}
+
+	@Override
+	public Integer editarCliente(Cliente cliente) {
+		return clienteDao.editarCliente(cliente);
+	}
+
+	@Override
+	public Cliente obtenerClientePorId(Map<String, Integer> mapCliente) {
+		return clienteDao.obtenerClientePorId(mapCliente);
+	}
+
+	@Override
+	public Integer eliminarCliente(Map<String, Integer> mapCliente) {
+		return clienteDao.eliminarCliente(mapCliente);
+	}
+	
+	
 
 }
